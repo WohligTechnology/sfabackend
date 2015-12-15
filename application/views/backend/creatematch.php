28dd6f20d19fa579a5d9bcb16210b4d7ae9465b0
Pooja <pooja.wohlig@gmail.com>
2015-12-15 11:17:51 +0530

    changes

--- application/views/backend/creatematch.php
+++ application/views/backend/creatematch.php
@@ -19,6 +19,7 @@
                                 <input type="text" id="normal-field" class="form-control" name="name" value='<?php echo set_value(' name ');?>'>
                             </div>
                         </div>
+<!--
                         <div class=" form-group">
                             <label class="col-sm-2 control-label" for="normal-field">Sports</label>
                             <div class="col-sm-4">
@@ -29,6 +30,21 @@
                             <label class="col-sm-2 control-label" for="normal-field">Sports Category</label>
                             <div class="col-sm-4">
                                 <?php echo form_dropdown("sportscategory",$sportscategory,set_value('sportscategory'),"class='chzn-select form-control'");?>
+                            </div>
+                        </div>
+-->
+                         <div class=" form-group">
+                            <label class="col-sm-2 control-label" for="normal-field">Sport</label>
+                            <div class="col-sm-4">
+                                <?php echo form_dropdown("sports",$sports,set_value('sports'),'id="sportid" class="test chzn-select form-control"');?>
+                            </div>
+                        </div>
+                        <div class=" form-group">
+                            <label class="col-sm-2 control-label" for="normal-field">Sport</label>
+                            <div class="col-sm-4">
+                                <select class="sportscategory form-control" name="sportscategory">
+
+                                </select>
                             </div>
                         </div>
                         <div class=" form-group">
@@ -93,3 +109,25 @@
         </section>
         </div>
     </div>
+     <script>
+        function populate(data, $select) {
+            $select.html("");
+            $select.append("<option value=''>Choose Sport Category</option>");
+            for (var i = 0; i < data.length; i++) {
+                $select.append("<option value='" + data[i].id + "'>" + data[i].name + "</option>");
+            }
+        }
+        $(document).ready(function () {
+
+            var $sportscategory = $("select.sportscategory");
+            var new_base_url = "<?php echo site_url(); ?>";
+            $("#sportid").change(function () {
+                $.getJSON(new_base_url + '/site/getSportCategoryBySport', {
+                    sport: $('select[name=sports]').val()
+                }, function (data) {
+                    console.log("abc");
+                    populate(data, $sportscategory);
+                });
+            });
+        });
+    </script>
